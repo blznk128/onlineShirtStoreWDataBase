a809d010d8f002cb14fc70930b723341e13e2814
Benny Borunda <blznk128@yahoo.com>
2022-01-19 18:17:26 -0700

    added folder structure

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+const PORT = process.env.PORT || 8080 ;
+const db = require('./models');
+const session = require('express-session');
+
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static("public"));
+
+// require('./routes/apiRoute')(app);
+require('./routes/htmlRoute')(app);
+
+db.sequelize.sync( { force: false } ).then(function() {
+    app.listen(PORT, () => {
+        console.log("app is listening on: ", PORT)
+    })
+})
