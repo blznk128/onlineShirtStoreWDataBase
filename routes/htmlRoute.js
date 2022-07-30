614276c21223c7dfa1e1966796b3d1f948b80c60
Benny <blznk128@yahoo.com>
2022-07-30 14:47:50 -0600

    added more color

--- routes/htmlRoute.js
+++ routes/htmlRoute.js
@@ -4,5 +4,8 @@ module.exports = function(app) {
     app.get("/", (req, res) => {
         res.sendFile(path.join(__dirname, "../public/index.html"))
     });
+    app.get("/shirts", (req, res) => {
+        res.sendFile(path.join(__dirname, "../public/shirts.html"))
+    });
 
 }
